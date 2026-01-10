7743b67125089bba3488835d68c6d542515f5100
Arman Ossi Loko <armanossiloko@outlook.com>
2026-01-10 23:24:54 +0100

    Fixed vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ command, mode }) => {
     // Update this base path to match your GitHub repository name
     // For example, if your repo is "my-quran-app", change it to '/my-quran-app/'
     // For user/organization pages (username.github.io), use '/'
-    base: mode === 'production' ? '/Quran/' : '/',
+    base: mode === 'production' ? '/quran-web/' : '/',
   }
 })
 
